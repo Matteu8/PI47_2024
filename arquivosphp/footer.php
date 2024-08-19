370ee8b619a6c802634e7ee0afa68586ad79aadf
Mateus Vinicius <mateus2vinicius@gmail.com>
2024-08-19 17:02:04 -0300

    .

---
+++ arquivosphp/footer.php
@@ -0,0 +1,50 @@
+<?php 
+// Inclua no lugar certo para adicionar o rodapé //
+echo 
+        "<footer>
+            <div class='social-icons'>
+                
+            </div>
+            <p>&copy; 2024 Senac-PR. Todos os direitos reservados.</p>
+        </footer>"
+
+/* 
+            RODAPÉ AZUL
+    <style>
+        footer {
+        background-color: #004A8D;
+        color: #fff;
+        text-align: center;
+        padding: 20px;
+        position: relative;
+        bottom: 0;
+        width: 100%;
+      }
+      body {
+        font-family: Arial, sans-serif;
+        margin: 0;
+        padding: 0;
+      }  
+      footer a {
+        color: #fff;
+        text-decoration: none;
+      }
+      footer a:hover {
+        text-decoration: underline;
+      }
+      .footer-links {
+        margin: 10px 0;
+      }
+      .footer-links a {
+        margin: 0 10px;
+      }
+      .social-icons {
+        margin: 10px 0;
+      }
+      .social-icons a {
+        margin: 0 5px;
+      }
+    </style>
+*/
+
+?>
---
+++ arquivosphp/header.php
@@ -0,0 +1,10 @@
+<?php
+// Inclua no lugar certo para adicionar o cabecabeçalho //
+
+echo "<div class='row visible-md visible-lg' style='background-color:#3a6da1;'>
+    <div class='col-md-5' style='background-color:#3a6da1; margin-right:0px; margin-left:0px'>
+        <a href='/principal/'><img src='img/topo_site_bl1_2018.png' class='img img-responsive'></a>
+    </div>
+</div>"
+
+?>
