28521758597166272f5c1d50f072aba01031b23a
ANDRE LUIZ FRANCISCONI BUENO <andre.2890@aluno.pr.senac.br>
2024-09-02 15:21:58 -0300

    msg

--- teste_andre/login.php
+++ teste_andre/login.php
@@ -5,192 +5,168 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
 
-
-    <script>link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" 
+    <!-- Importando Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" 
     rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
-integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
-<style>
-.container {
-  max-width: 350px;
-  background: #f8f9fd;
-  background: linear-gradient(
-    0deg,
-    rgb(255, 255, 255) 0%,
-    rgb(244, 247, 251) 100%
-  );
-  border-radius: 40px;
-  padding: 25px 35px;
-  border: 5px solid rgb(255, 255, 255);
-  box-shadow: rgba(133, 189, 215, 0.8784313725) 0px 30px 30px -20px;
-  margin: 20px;
-}
+    <style>
+        .form {
+            display: flex;
+            flex-direction: column;
+            gap: 10px;
+            max-width: 350px;
+            background-color: #f1f1f1;
+            padding: 20px;
+            border-radius: 20px;
+            position: relative;
+        }
 
-.heading {
-  text-align: center;
-  font-weight: 900;
-  font-size: 30px;
-  color: rgb(16, 137, 211);
-}
+        .title {
+            font-size: 28px;
+            color: royalblue;
+            font-weight: 600;
+            letter-spacing: -1px;
+            position: relative;
+            display: flex;
+            align-items: center;
+            padding-left: 30px;
+        }
 
-.form {
-  margin-top: 20px;
-}
+        .title::before, .title::after {
+            position: absolute;
+            content: "";
+            height: 16px;
+            width: 16px;
+            border-radius: 50%;
+            left: 0px;
+            background-color: royalblue;
+        }
 
-.form .input {
-  width: 100%;
-  background: white;
-  border: none;
-  padding: 15px 20px;
-  border-radius: 20px;
-  margin-top: 15px;
-  box-shadow: #cff0ff 0px 10px 10px -5px;
-  border-inline: 2px solid transparent;
-}
+        .title::before {
+            width: 18px;
+            height: 18px;
+            background-color: royalblue;
+        }
 
-.form .input::-moz-placeholder {
-  color: rgb(170, 170, 170);
-}
+        .title::after {
+            width: 18px;
+            height: 18px;
+            animation: pulse 1s linear infinite;
+        }
 
-.form .input::placeholder {
-  color: rgb(170, 170, 170);
-}
+        .message, .signin {
+            color: rgba(88, 87, 87, 0.822);
+            font-size: 14px;
+        }
 
-.form .input:focus {
-  outline: none;
-  border-inline: 2px solid #12b1d1;
-}
+        .signin {
+            text-align: center;
+        }
 
-.form .forgot-password {
-  display: block;
-  margin-top: 10px;
-  margin-left: 10px;
-}
+        .signin a {
+            color: royalblue;
+        }
 
-.form .forgot-password a {
-  font-size: 11px;
-  color: #0099ff;
-  text-decoration: none;
-}
+        .signin a:hover {
+            text-decoration: underline royalblue;
+        }
 
-.form .login-button {
-  display: block;
-  width: 100%;
-  font-weight: bold;
-  background: linear-gradient(
-    45deg,
-    rgb(16, 137, 211) 0%,
-    rgb(18, 177, 209) 100%
-  );
-  color: white;
-  padding-block: 15px;
-  margin: 20px auto;
-  border-radius: 20px;
-  box-shadow: rgba(133, 189, 215, 0.8784313725) 0px 20px 10px -15px;
-  border: none;
-  transition: all 0.2s ease-in-out;
-}
+        .flex {
+            display: flex;
+            width: 100%;
+            gap: 6px;
+        }
 
-.form .login-button:hover {
-  transform: scale(1.03);
-  box-shadow: rgba(133, 189, 215, 0.8784313725) 0px 23px 10px -20px;
-}
+        .form label {
+            position: relative;
+        }
 
-.form .login-button:active {
-  transform: scale(0.95);
-  box-shadow: rgba(133, 189, 215, 0.8784313725) 0px 15px 10px -10px;
-}
+        .form label .input {
+            width: 100%;
+            padding: 10px 10px 20px 10px;
+            outline: 0;
+            border: 1px solid rgba(105, 105, 105, 0.397);
+            border-radius: 10px;
+        }
 
-.social-account-container {
-  margin-top: 25px;
-}
+        .form label .input + span {
+            position: absolute;
+            left: 10px;
+            top: 15px;
+            color: grey;
+            font-size: 0.9em;
+            cursor: text;
+            transition: 0.3s ease;
+        }
 
-.social-account-container .title {
-  display: block;
-  text-align: center;
-  font-size: 10px;
-  color: rgb(170, 170, 170);
-}
+        .form label .input:placeholder-shown + span {
+            top: 15px;
+            font-size: 0.9em;
+        }
 
-.social-account-container .social-accounts {
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  gap: 15px;
-  margin-top: 5px;
-}
+        .form label .input:focus + span, .form label .input:valid + span {
+            top: 30px;
+            font-size: 0.7em;
+            font-weight: 600;
+        }
 
-.social-account-container .social-accounts .social-button {
-  background: linear-gradient(45deg, rgb(0, 0, 0) 0%, rgb(112, 112, 112) 100%);
-  border: 5px solid white;
-  padding: 5px;
-  border-radius: 50%;
-  width: 40px;
-  aspect-ratio: 1;
-  display: grid;
-  place-content: center;
-  box-shadow: rgba(133, 189, 215, 0.8784313725) 0px 12px 10px -8px;
-  transition: all 0.2s ease-in-out;
-}
+        .form label .input:valid + span {
+            color: green;
+        }
 
-.social-account-container .social-accounts .social-button .svg {
-  fill: white;
-  margin: auto;
-}
+        .submit {
+            border: none;
+            outline: none;
+            background-color: royalblue;
+            padding: 10px;
+            border-radius: 10px;
+            color: #fff;
+            font-size: 16px;
+            transform: .3s ease;
+        }
 
-.social-account-container .social-accounts .social-button:hover {
-  transform: scale(1.2);
-}
+        .submit:hover {
+            background-color: rgb(56, 90, 194);
+        }
 
-.social-account-container .social-accounts .social-button:active {
-  transform: scale(0.9);
-}
-
-.agreement {
-  display: block;
-  text-align: center;
-  margin-top: 15px;
-}
-
-.agreement a {
-  text-decoration: none;
-  color: #0099ff;
-  font-size: 9px;
-}
-
-  </style>
-
+        @keyframes pulse {
+            from {
+                transform: scale(0.9);
+                opacity: 1;
+            }
+            to {
+                transform: scale(1.8);
+                opacity: 0;
+            }
+        }
+    </style>
 </head>
 <body>
-<div class="container">
-  <div class="heading">Login in</div>
-  <form class="form" action="">
-    <input
-      placeholder="E-mail"
-      id="email"
-      name="email"
-      type="email"
-      class="input"
-      required=""
-    />
-    <input
-      placeholder="Senha"
-      id="password"
-      name="password"
-      type="password"
-      class="input"
-      required=""
-    />
-    <span class="forgot-password"><a href="#">Esqueceu a senha?</a></span>
-    <input value="Sign In" type="submit" class="login-button" />
-  </form>
-  <div class="social-account-container">
-    <span class="title">Ou faça login com:</span>
-    
-
-  <span class="agreement"><a href="#">Contrato de licença de usuário do Lear</a></span>
-</div>
+  <div class="d-flex justify-content-center" >
+<form class="form">
+    <p class="title">Login </p>
+    <p class="message">Ja possui uma conta; se ja possuir entre.</p>
+        <div class="form">
+        
+            
+    <label>
+        <input  placeholder="Email" type="email" class="input" required>
+        <span></span>
+    </label> 
+        
+    <label>
+        <input required="" placeholder="Senha" type="senha" class="input" required>
+        <span></span>
+    </label>
+    <label>
+      
+    </label>
+    <button class="submit">Entrar</button>
+    <p class="signin">Nao se cadastrou ainda. <a href="#">Cadastre-se aqui</a> </p>
+    </div>
+</form>
 
 
 </body>
