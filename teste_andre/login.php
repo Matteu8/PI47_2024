1d1a94f5fdb0ea462f754d3bac608a427f46d6f6
ANDRE LUIZ FRANCISCONI BUENO <andre.2890@aluno.pr.senac.br>
2024-09-02 16:11:02 -0300

    msg

--- teste_andre/login.php
+++ teste_andre/login.php
@@ -8,140 +8,12 @@
     <!-- Importando Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" 
     rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
     integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="login.css">
 
-    <style>
-        .form {
-            display: flex;
-            flex-direction: column;
-            gap: 10px;
-            max-width: 350px;
-            background-color: #f1f1f1;
-            padding: 20px;
-            border-radius: 20px;
-            position: relative;
-        }
 
-        .title {
-            font-size: 28px;
-            color: royalblue;
-            font-weight: 600;
-            letter-spacing: -1px;
-            position: relative;
-            display: flex;
-            align-items: center;
-            padding-left: 30px;
-        }
-
-        .title::before, .title::after {
-            position: absolute;
-            content: "";
-            height: 16px;
-            width: 16px;
-            border-radius: 50%;
-            left: 0px;
-            background-color: royalblue;
-        }
-
-        .title::before {
-            width: 18px;
-            height: 18px;
-            background-color: royalblue;
-        }
-
-        .title::after {
-            width: 18px;
-            height: 18px;
-            animation: pulse 1s linear infinite;
-        }
-
-        .message, .signin {
-            color: rgba(88, 87, 87, 0.822);
-            font-size: 14px;
-        }
-
-        .signin {
-            text-align: center;
-        }
-
-        .signin a {
-            color: royalblue;
-        }
-
-        .signin a:hover {
-            text-decoration: underline royalblue;
-        }
-
-        .flex {
-            display: flex;
-            width: 100%;
-            gap: 6px;
-        }
-
-        .form label {
-            position: relative;
-        }
-
-        .form label .input {
-            width: 100%;
-            padding: 10px 10px 20px 10px;
-            outline: 0;
-            border: 1px solid rgba(105, 105, 105, 0.397);
-            border-radius: 10px;
-        }
-
-        .form label .input + span {
-            position: absolute;
-            left: 10px;
-            top: 15px;
-            color: grey;
-            font-size: 0.9em;
-            cursor: text;
-            transition: 0.3s ease;
-        }
-
-        .form label .input:placeholder-shown + span {
-            top: 15px;
-            font-size: 0.9em;
-        }
-
-        .form label .input:focus + span, .form label .input:valid + span {
-            top: 30px;
-            font-size: 0.7em;
-            font-weight: 600;
-        }
-
-        .form label .input:valid + span {
-            color: green;
-        }
-
-        .submit {
-            border: none;
-            outline: none;
-            background-color: royalblue;
-            padding: 10px;
-            border-radius: 10px;
-            color: #fff;
-            font-size: 16px;
-            transform: .3s ease;
-        }
-
-        .submit:hover {
-            background-color: rgb(56, 90, 194);
-        }
-
-        @keyframes pulse {
-            from {
-                transform: scale(0.9);
-                opacity: 1;
-            }
-            to {
-                transform: scale(1.8);
-                opacity: 0;
-            }
-        }
-    </style>
+        
 </head>
 <body>
   <div class="d-flex justify-content-center" >
@@ -157,7 +29,7 @@
     </label> 
         
     <label>
-        <input required="" placeholder="Senha" type="senha" class="input" required>
+        <input  placeholder="Senha" type="senha" class="input" required>
         <span></span>
     </label>
     <label>
