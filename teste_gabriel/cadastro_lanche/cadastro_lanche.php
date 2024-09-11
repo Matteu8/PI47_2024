f378b2d72785c5df0b685d2e8ab1a89bf77a2ad1
GABRIEL V HOROBINSKI <horobinski.3902@aluno.pr.senac.br>
2024-09-11 15:10:03 -0300

    arrumar

--- teste_gabriel/cadastro_lanche/cadastro_lanche.php
+++ teste_gabriel/cadastro_lanche/cadastro_lanche.php
@@ -9,7 +9,6 @@
         $mysqlierrno = "erro";
         
         
-        
         /* colocar o name * aqui está como foto */
         
         if (isset($_FILES["foto"]) && $_FILES["foto"]["error"] == 0) {
@@ -49,7 +48,6 @@
            
         } 
         
-         
                     
     }
 
@@ -62,78 +60,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>cadastro de Lanches</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="gabriell.css">
+
+    
 </head>
-<body>
+
     <!-- ####################################################################################### -->
     <div class="row visible-md visible-lg" style="background-color:#3a6da1;" >     
         <div class="col-md-5" style="background-color:#3a6da1; margin-right:0px; margin-left:0px">
             <a href="/principal/"><img src="img/topo_site_bl1_2018.png" class="img img-responsive"></a>
         </div>
     </div>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            margin: 0;
-            padding: 0;
-            background-color: #f4f4f4;
-        }
-        .container {
-            max-width: 1200px;
-            margin: 0 auto;
-            padding: 20px;
-        }
-        header {
-            background-color: #f7941d;
-            color: #fff;
-            padding: 10px 0;
-            text-align: center;
-        }
-        h1 {
-            margin: 0;
-        }
-        .product-list {
-            display: flex;
-            flex-wrap: wrap;
-            gap: 20px;
-        }
-        .product {
-            background-color: #fff;
-            border: 1px solid #ddd;
-            border-radius: 5px;
-            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-            padding: 15px;
-            flex: 1 1 calc(33.333% - 20px);
-            box-sizing: border-box;
-            text-align: center;
-        }
-        .product img {
-            max-width: 100%;
-            height: auto;
-            border-radius: 5px;
-        }
-        .product h2 {
-            margin: 10px 0;
-            font-size: 1.5em;
-        }
-        .product p {
-            font-size: 1em;
-            color: #666;
-        }
-        .product .price {
-            font-size: 1.2em;
-            color: #333;
-            margin-top: 10px;
-        }
-        footer {
-            background-color: #333;
-            color: #fff;
-            text-align: center;
-            padding: 10px 0;
-            position: fixed;
-            bottom: 0;
-            width: 100%;
-        }
-    </style>
+   
+    
+
 </head>
 <body>
     <header>
@@ -164,8 +107,11 @@
             <input class="btn btn-danger " type="reset" value="Voltar">
             
 
+            
         </form>
         <?php 
+        
+
         
         ?>
 </body>
