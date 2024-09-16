c0a314067b117d03a3668b4f60f3e6563e57d395
ARIELLA FERNANDA SANTOS BRAZ <ariella.7939@aluno.pr.senac.br>
2024-09-16 14:13:39 -0300

    cadastrodoces

--- teste_ariella/cadastrosobremesa.php
+++ teste_ariella/cadastrosobremesa.php
@@ -10,7 +10,7 @@
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $nome = $_POST['bt_nome'] ?? '';
     $preco = $_POST['bt_preco'] ?? '';
-    $quantidade = intval($_POST['bt_quantidade'] ?? 0);
+    $quantidade = intval(  $_POST['bt_quantidade'] ?? 0);
 
 
     if (isset($_FILES['bt_imagem'])) {
@@ -62,7 +62,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <title>Cadastro de sobremesa</title>
     <nav class="navbar fixed-top bg-body-tertiary">
         <div class="container-fluid">
-        
+        <link rel="stylesheet" href="ariella.css">
         </div>
       </nav>
 </head>
@@ -74,71 +74,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     </div>
 </div>
 
-<style>
-    body {
-        font-family: Arial, sans-serif;
-        margin: 0;
-        padding: 0;
-        background-color: #f4f4f4;
-    }
-    .container {
-        max-width: 1200px;
-        margin: 0 auto;
-        padding: 20px;
-    }
-    header {
-        background-color: #f7941d;
-        color: #fff;
-        padding: 10px 0;
-        text-align: center;
-    }
-    h1 {
-        margin: 0;
-    }
-    .product-list {
-        display: flex;
-        flex-wrap: wrap;
-        gap: 20px;
-    }
-    .product {
-        background-color: #fff;
-        border: 1px solid #ddd;
-        border-radius: 5px;
-        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-        padding: 15px;
-        flex: 1 1 calc(33.333% - 20px);
-        box-sizing: border-box;
-        text-align: center;
-    }
-    .product img {
-        max-width: 100%;
-        height: auto;
-        border-radius: 5px;
-    }
-    .product h2 {
-        margin: 10px 0;
-        font-size: 1.5em;
-    }
-    .product p {
-        font-size: 1em;
-        color: #666;
-    }
-    .product .price {
-        font-size: 1.2em;
-        color: #333;
-        margin-top: 10px;
-    }
-    footer {
-        background-color: #3a6da1;
-        color: #fff;
-        text-align: center;
-        padding: 10px 0;
-        position: fixed;
-        bottom: 0;
-        width: 100%;
-    }
-</style>    
-
 <body>
     <header>
         <h1>Formulário de sobremesa</h1>
@@ -149,7 +84,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             <label for="nome">Nome:</label>
             <input type="text" id="nome" name="bt_nome" required >
 
-            <label for="preco">preço</label>
+            <label for="preco">Preço</label>
             <input type="preco" id="preco" name="bt_preco" required>
 
             <label for="quantidade">Quantidade:</label>
