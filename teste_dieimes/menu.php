99b578938743b8f25a919bbc24282fa5e913d6a2
Dieimes Nunes <114628166+DieimesNunes@users.noreply.github.com>
2024-09-18 15:14:55 -0300

    aa

--- teste_dieimes/menu.php
+++ teste_dieimes/menu.php
@@ -1,42 +1,40 @@
-<div class="row">
-    <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #3a6da1;">
-        <div class="container-fluid">
-            <!-- Logo -->
-            <a class="navbar-brand" href="index.php">
-                <img src="img/topo_site_bl1_2018.png" class="img-fluid" alt="Logo">
-            </a>
+<nav class="navbar navbar-expand-lg" style="background-color: #3a6da1;">
+    <div class="container-fluid">
+        <!-- Logo com altura 100% -->
+        <a class="navbar-brand" href="index.php">
+            <img src="img/topo_site_bl1_2018.png" alt="Logo" class="img-fluid">
+        </a>
 
-            <!-- Botão de "hamburger" para dispositivos móveis -->
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+        <!-- Botão hamburger -->
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-            <!-- Itens do menu que serão mostrados/ocultados -->
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="index.php">Página Inicial</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="#">Lanches</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="#">Bebidas</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="#">Sobremesas</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="#">Pedidos</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="#">Sobre Nós</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="#">Sair</a>
-                    </li>
-                </ul>
-            </div>
+        <!-- Itens do menu -->
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="index.php">Página Inicial</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#">Lanches</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#">Bebidas</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#">Sobremesas</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#">Pedidos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#">Sobre Nós</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#">Sair</a>
+                </li>
+            </ul>
         </div>
-    </nav>
-</div>
+    </div>
+</nav>
