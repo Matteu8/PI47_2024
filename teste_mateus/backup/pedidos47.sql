3a1038f6aa2765a616a34c614bab7cfc99cfee09
Mateus <mateus2vinicius@gmail.com>
2024-09-21 13:24:58 -0300

    terminei tudo o que eu tinha sobre cliente/funcionario, cadastro cliente/funcionario e contato_andre +bd

--- teste_mateus/backup/pedidos47.sql
+++ teste_mateus/backup/pedidos47.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Tempo de geração: 18-Set-2024 às 19:25
--- Versão do servidor: 5.7.36
--- versão do PHP: 7.4.26
+-- Host: 127.0.0.1
+-- Tempo de geração: 21/09/2024 às 18:20
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -26,63 +26,44 @@ USE `pedidos47`;
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `bebidas`
+-- Estrutura para tabela `clientes`
 --
 
-DROP TABLE IF EXISTS `bebidas`;
-CREATE TABLE IF NOT EXISTS `bebidas` (
-  `id_bebidas` int(11) NOT NULL AUTO_INCREMENT,
-  `nome` varchar(50) NOT NULL,
-  `preco` varchar(50) NOT NULL,
-  `quantidade` varchar(50) NOT NULL,
-  PRIMARY KEY (`id_bebidas`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `clientes`
---
-
-DROP TABLE IF EXISTS `clientes`;
-CREATE TABLE IF NOT EXISTS `clientes` (
-  `id_clientes` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `clientes` (
+  `id_clientes` int(11) NOT NULL,
   `nome` varchar(200) NOT NULL,
   `curso` varchar(100) NOT NULL,
   `periodo` varchar(10) NOT NULL,
-  `telefone` varchar(15) NOT NULL,
+  `telefone` varchar(20) NOT NULL,
   `email` varchar(300) NOT NULL,
-  `senha` varchar(300) NOT NULL,
-  PRIMARY KEY (`id_clientes`)
-) ENGINE=MyISAM AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+  `senha` varchar(300) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
--- Extraindo dados da tabela `clientes`
+-- Despejando dados para a tabela `clientes`
 --
 
 INSERT INTO `clientes` (`id_clientes`, `nome`, `curso`, `periodo`, `telefone`, `email`, `senha`) VALUES
 (1, 'Arthur', 'ProgramaÃ§Ã£o web', 'Tarde', '43 4002-8922', 'arthur@gmail.com', ''),
-(13, 'Mateus Vinicius', 'ProgramaÃ§Ã£o WEB', 'Tarde', '43 1234567899', 'mateus.68998@aluno.pr.senac.br', '$2y$10$SZe70tBBVcC1mxegOxp2Ju33bmZeYvjaSHEMN3jI4yMWX.n/eoqBa');
+(7, 'Mateus Vinicius', 'Programação WEB', 'Tarde', '45416594844', 'mateus.68998@aluno.pr.senac.br', '$2y$10$eS4YwjqLPxXNbdxtArxWD.4uIGgg.azuE2/ZxI/H6KLkdyDoJeWvG');
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `contato`
+-- Estrutura para tabela `contato`
 --
 
-DROP TABLE IF EXISTS `contato`;
-CREATE TABLE IF NOT EXISTS `contato` (
-  `id_contato` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `contato` (
+  `id_contato` int(11) NOT NULL,
   `nome` varchar(200) NOT NULL,
   `email` varchar(100) NOT NULL,
   `telefone` varchar(12) NOT NULL,
   `assunto` varchar(30) NOT NULL,
-  `mensagem` varchar(500) NOT NULL,
-  PRIMARY KEY (`id_contato`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+  `mensagem` varchar(500) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
--- Extraindo dados da tabela `contato`
+-- Despejando dados para a tabela `contato`
 --
 
 INSERT INTO `contato` (`id_contato`, `nome`, `email`, `telefone`, `assunto`, `mensagem`) VALUES
@@ -91,49 +72,137 @@ INSERT INTO `contato` (`id_contato`, `nome`, `email`, `telefone`, `assunto`, `me
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `funcionarios`
+-- Estrutura para tabela `feedback`
 --
 
-DROP TABLE IF EXISTS `funcionarios`;
-CREATE TABLE IF NOT EXISTS `funcionarios` (
-  `id_funcionario` int(11) NOT NULL AUTO_INCREMENT,
-  `nome` varchar(200) NOT NULL,
+CREATE TABLE `feedback` (
+  `id_feedback` int(11) NOT NULL,
+  `nome` varchar(300) NOT NULL,
   `email` varchar(300) NOT NULL,
-  `senha` varchar(300) NOT NULL,
-  PRIMARY KEY (`id_funcionario`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+  `telefone` varchar(50) NOT NULL,
+  `assunto` varchar(100) NOT NULL,
+  `msg` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
--- Extraindo dados da tabela `funcionarios`
+-- Despejando dados para a tabela `feedback`
 --
 
-INSERT INTO `funcionarios` (`id_funcionario`, `nome`, `email`, `senha`) VALUES
-(2, 'Ronaldo FenÃ´meno', '1@1', '$2y$10$EAwcvd4aJy8lcpO7AY4xqe7i1vec6N1k8NNs7GtVY95/0FbtajBD.');
+INSERT INTO `feedback` (`id_feedback`, `nome`, `email`, `telefone`, `assunto`, `msg`) VALUES
+(1, 'a', 'a@a', 'a', 'Reclamação', 'a'),
+(2, 'Robson', 'robson@robson', '43940028922', 'Dúvida', 'Tem pão velho?\r\n');
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `lanches`
+-- Estrutura para tabela `funcionarios`
 --
 
-DROP TABLE IF EXISTS `lanches`;
-CREATE TABLE IF NOT EXISTS `lanches` (
-  `id_lanches` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `funcionarios` (
+  `id_funcionario` int(11) NOT NULL,
+  `nome` varchar(300) NOT NULL,
+  `email` varchar(300) NOT NULL,
+  `senha` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Despejando dados para a tabela `funcionarios`
+--
+
+INSERT INTO `funcionarios` (`id_funcionario`, `nome`, `email`, `senha`) VALUES
+(3, 'Mateus', '1@1', '$2y$10$V2LqQBj3gAaKxJPZkSQKhuQqeGtdyRdqGHPUtsZHDcZk4WdY8kz/y'),
+(5, '1', '2@2', '$2y$10$K.lpKHU3yElnOdWgRAOd5.HSkkymXS9XifFQ5xyV48ECTibv25k0i');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `lanches`
+--
+
+CREATE TABLE `lanches` (
+  `id_lanches` int(11) NOT NULL,
   `nome` varchar(50) NOT NULL,
   `ingredientes` varchar(300) NOT NULL,
   `preco` varchar(20) NOT NULL,
-  `foto` varchar(300) NOT NULL,
-  PRIMARY KEY (`id_lanches`)
-) ENGINE=MyISAM AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
+  `foto` varchar(300) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
--- Extraindo dados da tabela `lanches`
+-- Despejando dados para a tabela `lanches`
 --
 
 INSERT INTO `lanches` (`id_lanches`, `nome`, `ingredientes`, `preco`, `foto`) VALUES
 (1, 'X-tudo', 'Tem tudo e mais um pouco', 'R$ 19,99', ''),
 (11, 'PÃ£o com ovo', 'pÃ£o e ovo frito', 'R$ 3,99', 'Pao-com-Ovo.jpg'),
 (13, 'PÃ£o com ovo', 'pÃ£o e ovo frito', 'R$ 3,99', 'as-logo-design-template-6cb212472d4c9fdbc0784695e6464e6f_screen.jpg');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `clientes`
+--
+ALTER TABLE `clientes`
+  ADD PRIMARY KEY (`id_clientes`);
+
+--
+-- Índices de tabela `contato`
+--
+ALTER TABLE `contato`
+  ADD PRIMARY KEY (`id_contato`);
+
+--
+-- Índices de tabela `feedback`
+--
+ALTER TABLE `feedback`
+  ADD PRIMARY KEY (`id_feedback`);
+
+--
+-- Índices de tabela `funcionarios`
+--
+ALTER TABLE `funcionarios`
+  ADD PRIMARY KEY (`id_funcionario`);
+
+--
+-- Índices de tabela `lanches`
+--
+ALTER TABLE `lanches`
+  ADD PRIMARY KEY (`id_lanches`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `clientes`
+--
+ALTER TABLE `clientes`
+  MODIFY `id_clientes` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT de tabela `contato`
+--
+ALTER TABLE `contato`
+  MODIFY `id_contato` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de tabela `feedback`
+--
+ALTER TABLE `feedback`
+  MODIFY `id_feedback` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de tabela `funcionarios`
+--
+ALTER TABLE `funcionarios`
+  MODIFY `id_funcionario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de tabela `lanches`
+--
+ALTER TABLE `lanches`
+  MODIFY `id_lanches` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
