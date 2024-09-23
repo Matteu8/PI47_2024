5f65d9ad51658046d683d54ce6e27605d1dccd91
Dieimes Nunes <114628166+DieimesNunes@users.noreply.github.com>
2024-09-23 16:05:49 -0300

    menu

---
+++ teste_dieimes/menu.php
@@ -0,0 +1,41 @@
+
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #3a6da1;">
+        <div class="container-fluid">
+            <!-- Logo -->
+            <a class="navbar-brand" href="index.php">
+                <img src="img/topo_site_bl1_2018.png" class="img-fluid" alt="Logo">
+            </a>
+
+            <!-- Botão de "hamburger" para dispositivos móveis -->
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <!-- Itens do menu que serão mostrados/ocultados -->
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="index.php">Página Inicial</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#">Lanches</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#">Bebidas</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#">Sobremesas</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#">Pedidos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#">Sobre Nós</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#">Sair</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
