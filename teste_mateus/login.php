052255eea2533dfec8932bc8744a204a2811c1b6
MATEUS VINICIUS ALVES DA SILVA <mateus.68998@aluno.pr.senac.br>
2024-09-25 13:58:40 -0300

    ab

--- teste_mateus/login.php
+++ teste_mateus/login.php
@@ -71,7 +71,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   <title>Login</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-  <link rel="stylesheet" href="ariella.css">
+  <link rel="stylesheet" href="gabriell.css">
 </head>
 
 <body>
