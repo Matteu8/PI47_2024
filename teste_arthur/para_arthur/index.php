935df4d18b2f3bb40be8670e8b76e2dd757c2a21
ARTHUR SCHIMANSKI VIEIRA <arthur.3933@aluno.pr.senac.br>
2024-10-02 16:05:49 -0300

    final da aula

---
+++ teste_arthur/para_arthur/index.php
@@ -0,0 +1,129 @@
+<?php
+    include("conexao.php");
+    
+    if ((!isset($_SESSION))){
+        session_start();
+    }
+    
+    $bebidas = "SELECT * FROM bebidas";
+    $lanches = "SELECT * FROM lanches";
+    //$sobremesa = "SELECT * FROM sobremesas";
+
+    $bebidas_result = $mysqli->query($bebidas) or die($mysqli->error);
+    $lanches_result = $mysqli->query($lanches) or die($mysqli->error);
+    //$sobremesa_result = $mysqli->query($sobremesa) or die($mysqli->error);
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bebidas</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="dieimes.css">
+    <style>
+        body { background-color: #f8f9fa; }
+        h1 { font-size: 28px; font-weight: bold; margin-bottom: 20px; text-align: center; color: #333; }
+        .row { margin-bottom: 20px; }
+        .card { transition: transform 0.3s; }
+        .card:hover { transform: scale(1.05); }
+        .card-img-top { border-radius: 8px; height: 300px; object-fit: cover; }
+        .btn-primary { background-color: #007bff; border: none; transition: background-color 0.3s ease; }
+        .btn-primary:hover { background-color: #0056b3; }
+        .btn { border-radius: 5px; padding: 10px 15px; font-size: 14px; }
+        .img-thumbnail { width: 100px; height: 100px; object-fit: cover; border-radius: 50%; }
+    </style>
+</head>
+<body>
+    <?php include "menu.php"; ?>
+    
+    <h1 class="mt-5">Cardapio</h1>
+    
+    <h1 class="mt-5">Lanches</h1>
+    <!-- Lanches -->
+    <div class="container">
+        <div class="row">
+            <?php while ($lanche = $lanches_result->fetch_assoc()) { ?>
+                <div class="col-md-3 mb-4"> 
+                    <div class="card">
+                        <img src="<?php echo htmlspecialchars($lanche['foto']); ?>" class="card-img-top" alt="<?php echo htmlspecialchars($lanche['nome']); ?>">
+                        <div class="card-body">
+                            <h5 class="card-title"><?php echo htmlspecialchars($lanche['nome']); ?></h5>
+                            <p class="card-text">Tipo: <?php echo htmlspecialchars($lanche['ingredientes']); ?></p>
+                            <p class="card-text">R$ <?php echo number_format($lanche['preco'], 2, ',', '.'); ?></p>
+
+                            
+                            <?php if (isset($_SESSION['tipo_usuario'])):?>
+                                <a href="lanches.php" class="btn btn-primary">sim</a>
+                             <?php else: ?>
+                                <div class="alert alert-danger" role="alert">Você não esta logado, faça login para pedir</div>
+                                <a href="login.php" class="btn btn-primary">Logar</a>
+                                <?php endif; ?>
+                        </div>
+                    </div>
+                </div>
+            <?php } ?>
+        </div>
+    </div>
+
+    <h1 class="mt-5">Bebidas</h1>
+    <!-- Bebidas -->
+    <div class="container">
+        <div class="row">
+            <?php while ($bebida = $bebidas_result->fetch_assoc()) { ?>
+                <div class="col-md-3 mb-4"> 
+                    <div class="card">
+                        <img src="<?php echo htmlspecialchars($bebida['foto']); ?>" class="card-img-top" alt="<?php echo htmlspecialchars($bebida['nome']); ?>">
+                        <div class="card-body">
+                            <h5 class="card-title"><?php echo htmlspecialchars($bebida['nome']); ?></h5>
+                            <p class="card-text">Tipo: <?php echo htmlspecialchars($bebida['tipo']); ?></p>
+                            <p class="card-text">R$ <?php echo number_format($bebida['preco'], 2, ',', '.'); ?></p>
+
+                            <?php if (isset($_SESSION['tipo_usuario'])):?>
+                                <a href="lanches.php" class="btn btn-primary">sim</a>
+                             <?php else: ?>
+                                <div class="alert alert-danger" role="alert">Você não esta logado, faça login para pedir</div>
+                                <a href="login.php" class="btn btn-primary">Logar</a>
+                                <?php endif; ?>
+                        </div>
+                    </div>
+                </div>
+            <?php } ?>
+        </div>
+    </div>
+
+    
+
+    <h1 class="mt-5">Sobremesas</h1>
+
+    <!-- Sobremesas -->
+    <div class="container">
+        <div class="row">
+            <?php while ($sobremesa = $sobremesa_result->fetch_assoc()) { ?>
+                <div class="col-md-3 mb-4"> 
+                    <div class="card">
+                        <img src="<?php echo htmlspecialchars($sobremesa['foto']); ?>" class="card-img-top" alt="<?php echo htmlspecialchars($sobremesa['nome']); ?>">
+                        <div class="card-body">
+                            <h5 class="card-title"><?php echo htmlspecialchars($sobremesa['nome']); ?></h5>
+                            <p class="card-text">Tipo: <?php echo htmlspecialchars($sobremesa['tipo']); ?></p>
+                            <p class="card-text">R$ <?php echo number_format($sobremesa['preco'], 2, ',', '.'); ?></p>
+
+
+                            <?php if (isset($_SESSION['tipo_usuario'])):?>
+                                <a href="lanches.php" class="btn btn-primary">sim</a>
+                             <?php else: ?>
+                                <div class="alert alert-danger" role="alert">Você não esta logado, faça login para pedir</div>
+                                <a href="login.php" class="btn btn-primary">Logar</a>
+                                <?php endif; ?>
+                        </div>
+                    </div>
+                </div>
+            <?php } ?>
+        </div>
+    </div>
+    <br><br><br><br><br><br>
+    <?php include "rodape.php"; ?>
+</body>
+</html>
---
+++ teste_arthur/para_arthur/pedidos47.sql
@@ -0,0 +1,253 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Tempo de geração: 30-Set-2024 às 17:38
+-- Versão do servidor: 5.7.36
+-- versão do PHP: 7.4.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `pedidos47`
+--
+CREATE DATABASE IF NOT EXISTS `pedidos47` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `pedidos47`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `bebidas`
+--
+
+DROP TABLE IF EXISTS `bebidas`;
+CREATE TABLE IF NOT EXISTS `bebidas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  `tipo` varchar(255) NOT NULL,
+  `preco` decimal(10,2) NOT NULL,
+  `quantidade` int(11) NOT NULL,
+  `foto` varchar(300) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `bebidas`
+--
+
+INSERT INTO `bebidas` (`id`, `nome`, `tipo`, `preco`, `quantidade`, `foto`) VALUES
+(1, 'Suco de Laranja', 'Suco no copo', '2.99', 20, 'receber/66f46256722a0.png'),
+(2, 'Guarana Jesus', 'Lata', '5.00', 30, 'receber/66f1aec1a37c9.jpg'),
+(3, 'Del vale', 'Garafinha de suco ', '5.00', 20, 'receber/66f1b0ee41751.jpg'),
+(4, 'CafÃ©', 'Copo descartavel ', '1.00', 50, 'receber/66f1b15815108.png'),
+(5, 'Pepsi', 'Lata', '5.00', 20, 'receber/66f1b1bdb6fa7.png'),
+(6, 'Coca Cola', 'Lata', '5.00', 20, 'receber/66f1b1e03b8dd.png');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `clientes`
+--
+
+DROP TABLE IF EXISTS `clientes`;
+CREATE TABLE IF NOT EXISTS `clientes` (
+  `id_clientes` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(200) NOT NULL,
+  `curso` varchar(100) NOT NULL,
+  `periodo` varchar(10) NOT NULL,
+  `telefone` varchar(20) NOT NULL,
+  `email` varchar(300) NOT NULL,
+  `senha` varchar(300) NOT NULL,
+  PRIMARY KEY (`id_clientes`)
+) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `clientes`
+--
+
+INSERT INTO `clientes` (`id_clientes`, `nome`, `curso`, `periodo`, `telefone`, `email`, `senha`) VALUES
+(1, 'Arthur', 'ProgramaÃ§Ã£o web', 'Tarde', '43 4002-8922', 'arthur@gmail.com', ''),
+(7, 'Mateus Vinicius', 'Programação WEB', 'Tarde', '45416594844', 'mateus.68998@aluno.pr.senac.br', '$2y$10$eS4YwjqLPxXNbdxtArxWD.4uIGgg.azuE2/ZxI/H6KLkdyDoJeWvG'),
+(13, 'onze', '1', '1', '1', '11@11', '$2y$10$hsri1ZM44htofnhPGpZrleDgmYIshuIQ55wY2eUxazkNRiVnE9LkS');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `contato`
+--
+
+DROP TABLE IF EXISTS `contato`;
+CREATE TABLE IF NOT EXISTS `contato` (
+  `id_contato` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(200) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `telefone` varchar(12) NOT NULL,
+  `assunto` varchar(30) NOT NULL,
+  `mensagem` varchar(500) NOT NULL,
+  PRIMARY KEY (`id_contato`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `contato`
+--
+
+INSERT INTO `contato` (`id_contato`, `nome`, `email`, `telefone`, `assunto`, `mensagem`) VALUES
+(1, 'Arthur', 'arthur@gmail.com', '43 4002-8922', 'Elogio', 'Gostei dos Lanches, mas poderia ser um pouco menos quente o lanche.');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `feedback`
+--
+
+DROP TABLE IF EXISTS `feedback`;
+CREATE TABLE IF NOT EXISTS `feedback` (
+  `id_feedback` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(300) NOT NULL,
+  `email` varchar(300) NOT NULL,
+  `telefone` varchar(50) NOT NULL,
+  `assunto` varchar(100) NOT NULL,
+  `msg` varchar(300) NOT NULL,
+  PRIMARY KEY (`id_feedback`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `funcionarios`
+--
+
+DROP TABLE IF EXISTS `funcionarios`;
+CREATE TABLE IF NOT EXISTS `funcionarios` (
+  `id_funcionario` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(300) NOT NULL,
+  `email` varchar(300) NOT NULL,
+  `senha` varchar(300) NOT NULL,
+  PRIMARY KEY (`id_funcionario`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `funcionarios`
+--
+
+INSERT INTO `funcionarios` (`id_funcionario`, `nome`, `email`, `senha`) VALUES
+(3, 'Mateus', '1@1', '$2y$10$V2LqQBj3gAaKxJPZkSQKhuQqeGtdyRdqGHPUtsZHDcZk4WdY8kz/y'),
+(5, '1', '2@2', '$2y$10$K.lpKHU3yElnOdWgRAOd5.HSkkymXS9XifFQ5xyV48ECTibv25k0i');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `itens_pedido`
+--
+
+DROP TABLE IF EXISTS `itens_pedido`;
+CREATE TABLE IF NOT EXISTS `itens_pedido` (
+  `id_item_pedido` int(11) NOT NULL AUTO_INCREMENT,
+  `id_pedido` int(11) DEFAULT NULL,
+  `id_lanches` int(11) DEFAULT NULL,
+  `id` int(11) DEFAULT NULL,
+  `id_sobremesa` int(11) DEFAULT NULL,
+  `quantidade` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_item_pedido`),
+  KEY `fk_pedido` (`id_pedido`),
+  KEY `fk_lanches` (`id_lanches`),
+  KEY `fk_bebidas` (`id`),
+  KEY `fk_sobremesa` (`id_sobremesa`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `itens_pedido`
+--
+
+INSERT INTO `itens_pedido` (`id_item_pedido`, `id_pedido`, `id_lanches`, `id`, `id_sobremesa`, `quantidade`) VALUES
+(1, 1, 1, NULL, NULL, 1),
+(2, 2, 1, NULL, NULL, 8);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `lanches`
+--
+
+DROP TABLE IF EXISTS `lanches`;
+CREATE TABLE IF NOT EXISTS `lanches` (
+  `id_lanches` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) NOT NULL,
+  `ingredientes` varchar(300) NOT NULL,
+  `preco` varchar(20) NOT NULL,
+  `quantidade` int(11) NOT NULL,
+  `foto` varchar(300) NOT NULL,
+  PRIMARY KEY (`id_lanches`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `lanches`
+--
+
+INSERT INTO `lanches` (`id_lanches`, `nome`, `ingredientes`, `preco`, `quantidade`, `foto`) VALUES
+(1, 'SanduÃ­che', 'PÃ£o, Alface, Presunto, Tomate', '123', 10, 'Lanches/img/66f8426e0dd7b.jpg'),
+(2, 'Claudio', 'Ã“leo', '2', 10, 'Lanches/img/66f84302cbdf3.jpg'),
+(3, 'Hamburguer', '6', '20', 10, 'Lanches/img/66f8435124fce.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pedidos`
+--
+
+DROP TABLE IF EXISTS `pedidos`;
+CREATE TABLE IF NOT EXISTS `pedidos` (
+  `id_pedido` int(11) NOT NULL AUTO_INCREMENT,
+  `id_clientes` int(11) DEFAULT NULL,
+  `nome_funcionario` varchar(300) NOT NULL,
+  `data_pedido` date DEFAULT NULL,
+  `status` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id_pedido`),
+  KEY `fk_clientes` (`id_clientes`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `pedidos`
+--
+
+INSERT INTO `pedidos` (`id_pedido`, `id_clientes`, `nome_funcionario`, `data_pedido`, `status`) VALUES
+(1, 7, 'Mateus Vinicius', '2024-09-30', 'Pedido ConcluÃ­do');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `sobremesa`
+--
+
+DROP TABLE IF EXISTS `sobremesa`;
+CREATE TABLE IF NOT EXISTS `sobremesa` (
+  `id_sobremesa` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(300) NOT NULL,
+  `preco` varchar(300) NOT NULL,
+  `quantidade` int(11) NOT NULL,
+  `imagem` varchar(300) NOT NULL,
+  PRIMARY KEY (`id_sobremesa`)
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `sobremesa`
+--
+
+INSERT INTO `sobremesa` (`id_sobremesa`, `nome`, `preco`, `quantidade`, `imagem`) VALUES
+(3, 'bolo 222', '85', 1, ''),
+(4, 'brigadeiro', '5', 1, 'recebidos.img/66e88044bd4a7.webp'),
+(6, 'bolo de cenoura', '5', 1, 'recebidos.img/66e880715d398.jpg');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
